6cc21f257073f1a3b7537b0f8cf822040973830d
Josiah Hands <josiahwhands@gmail.com>
2026-09-14 18:38:03 -0500

    tool1 complete

---
+++ app.ts
@@ -0,0 +1,46 @@
+let timeArr: number[] = []
+let course: number = timeArr[0];
+const courseTime = (time: number): void => {
+    timeArr.push(time);
+}
+
+
+const time = (course: number, time: number): void | string => {
+    const times: string =  (time/course).toFixed(1)
+    if (time < course) {
+        console.log(' ')
+        console.log("You're super fast! "+(course/time).toFixed(1)+" times faster than the course")
+        console.log('Course time = '+course)
+        console.log('Your time = '+time)
+        console.log(' ')
+    } else if (time >= course && time < course*2) {
+        console.log(' ')
+        console.log("You're fast! "+times+" times the course")
+        console.log('Course time = '+course)
+        console.log('Your time = '+time)
+        console.log(' ')
+    } else if (time >= course*2 && time < course*3) {
+        console.log(' ')
+        console.log("You're doing good! "+times+" times the course")
+        console.log('Course time = '+course)
+        console.log('Your time = '+time)
+        console.log(' ')
+    } else if (time >= course*3 && time < course*4) {
+        console.log(' ')
+        console.log("You're doing okay. "+times+" times the course")
+        console.log('Course time = '+course)
+        console.log('Your time = '+time)
+        console.log(' ')
+    } else if (time > course*4) {
+        console.log(' ')
+        console.log("You're slow. "+times+" times times course")
+        console.log('Course time = '+course)
+        console.log('Your time = '+time)
+        console.log(' ')
+    } else {
+        return 'error';
+    }
+}
+
+// enter the written time of the course and how long it took you (in hours). as in: time(course, you);
+time(1, 0.8);
